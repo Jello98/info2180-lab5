cd5f7b4351645479aa0bfdb5eee650ce012081cf
Jello98 <telljelani@gmail.com>
2020-11-19 23:23:33 -0500

    updated world.php

--- world.php
+++ world.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -14,7 +14,7 @@ $city= $_GET['city'];
 ?>
 
 <?php if(!(isset($_GET['context']))):?>
-
+<table>
 	<tr>
         <th>Country Name</th>
         <th>Continent</th> 
@@ -31,6 +31,7 @@ $city= $_GET['city'];
   </tr>
 <?php endforeach; ?>  
 </tbody>
+</table>
 
 <?php else : ?> 
 
